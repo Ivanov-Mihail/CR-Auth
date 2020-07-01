19674db1921a8cfa309bcd873760ac8bab34872f
Михаил Иванов <ivanov-mihail-3@live.com>
2020-07-01 10:42:30 +0300

    improvments

--- CR-Auth/Auth.cs
+++ CR-Auth/Auth.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,65 +16,64 @@ namespace CR_Auth
 {
 	public class Auth
 	{
-		public Auth(IConfiguration configuration)
+		private AuthUser _authUserData;
+		public string PublicKey { get; set; }
+		public Auth(string publicKey)
 		{
-			_configuration = configuration;
+			PublicKey = publicKey;
 		}
 
-		public List<PayloadRows> PayloadRows = new List<PayloadRows>();
 
-		public async Task<AuthUser> GetPayloadAfterValidation(string bearerToken)
+		public AuthUser GetPayloadWithoutTokenValidation(string bearerToken)
 		{
 			var token = bearerToken.Replace("Bearer ", "");
 			var jsonWebToken = new JsonWebToken(token);
 
-
-			// 1st implementation
-			AuthUser authUserData;
-			{
-				string id = null;
-				string[] roles = null;
-				string[] permissions = null;
-
-				jsonWebToken.TryGetPayloadValue<string>("id", out id);
-				jsonWebToken.TryGetPayloadValue<string[]>("roles", out roles);
-				jsonWebToken.TryGetPayloadValue<string[]>("permissions", out permissions);
-				authUserData = new AuthUser(id, roles, permissions);
-
-			}
-
-
-			var securityTokenHandler = new JwtSecurityTokenHandler();
-			var publicKey = _configuration["Jwt:Key"];
-			var options = GetTokenValidationParameters(publicKey);
-
-			var claimPrincipal = await Task.Run(() =>
-				securityTokenHandler.ValidateToken(bearerToken, options, out var securityToken));
-
-
-			if (null != claimPrincipal)
-			{
-				foreach (var claim in claimPrincipal.Claims)
-				{
-					var row = new PayloadRows
-					{
-						Key = claim.Type,
-						Value = claim.Value
-					};
-					PayloadRows.Add(row);
-				}
-			}
+			jsonWebToken.TryGetPayloadValue<string>("id", out string id);
+			jsonWebToken.TryGetPayloadValue<string[]>("roles", out var roles);
+			jsonWebToken.TryGetPayloadValue<string[]>("permissions", out var permissions);
+			var authUserData = new AuthUser(id, roles, permissions);
 
 			return authUserData;
-
 		}
 
-		private readonly IConfiguration _configuration;
+		public AuthUser GetPayloadFromValidatedToken(string bearerToken)
+		{
+			var options = GetTokenValidationParameters(PublicKey);
+			var token = bearerToken.Replace("Bearer ", "");
+			var securityTokenHandler = new JwtSecurityTokenHandler();
+			var claimPrincipal = securityTokenHandler.ValidateToken(token, options, out var securityToken);
+			//
+			if (claimPrincipal == null) return _authUserData;
+			_authUserData = new AuthUser();
+			//
+			var userId = claimPrincipal.Claims.First(claim => claim.Type == "id").Value;
+			_authUserData.UserId = userId;
+			//
+			var roles = claimPrincipal.Claims.Where(claim => claim.Type == "http://schemas.microsoft.com/ws/2008/06/identity/claims/role").ToArray();
+			var rolesLength = roles.Length;
+			_authUserData.Roles = new string[rolesLength];
+			for (var a = 0; a < rolesLength; a++)
+			{
+				_authUserData.Roles[a] = roles[a].Value;
+			}
+			//
+			var permissions = claimPrincipal.Claims.Where(claim => claim.Type == "permissions").ToArray();
+			var permissionsLength = permissions.Length;
+			_authUserData.Permissions = new string[permissionsLength];
+			for (var b = 0; b < permissionsLength; b++)
+			{
+				_authUserData.Permissions[b] = permissions[b].Value;
+			}
+			//
+			return _authUserData;
+		}
+
 		private TokenValidationParameters GetTokenValidationParameters(string key)
 		{
 
-			var rs256Token = key.Replace("-----\nBEGIN PUBLIC KEY-----", "");
-			rs256Token = rs256Token.Replace("\n-----END PUBLIC KEY-----", "");
+			var rs256Token = key.Replace("-----BEGIN PUBLIC KEY-----", "");
+			rs256Token = rs256Token.Replace("-----END PUBLIC KEY-----", "");
 
 			var keyBytes = Convert.FromBase64String(rs256Token);
 			var asymmetricKeyParameter = PublicKeyFactory.CreateKey(keyBytes);
